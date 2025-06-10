00c8ac4fbca7720f863e8fdf8f10c61cda7f983d
Peacebern <peace8282bern@gmail.com>
2025-06-10 22:16:38 +0100

    Update splice_array.js

--- tezting-js/advanced objects/splice_array.js
+++ tezting-js/advanced objects/splice_array.js
@@ -1,4 +1,5 @@
 arr = ["jason", "I", "does", "sability", "aryaStarr"];
 arr.splice(3, 1); //Note: 3 means start from index 3, while 1 means delete 1 items. so syntex is array.splice(start, howmanyTodelete);
 
-console.log(arr);
+console.log(arr); // [ 'jason', 'I', 'does', 'aryaStarr' ]
+
